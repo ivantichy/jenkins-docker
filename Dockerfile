29e8403f2d936e36abcf30cc859a355de64b7511
Ivan Tichy <ivan.tichy@gmail.com>
2016-07-22 14:04:57 +0200

    empty message

--- Dockerfile
+++ Dockerfile
@@ -9,5 +9,8 @@ apt-get install sudo -y -q && \
 apt-get install maven -y -q && \
 rm -rf /var/lib/apt/lists/*
 
+RUN groupadd docker -g 999 && usermod -G docker jenkins
+
+
 USER jenkins
 
--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-docker run -p 8082:8080 -p 50000:50000 -d -v /var/docker-data/jenkins-home:/var/jenkins_home ivantichy/jenkins
+docker run -p 8082:8080 -p 50000:50000 -d -v /var/docker-data/jenkins-home:/var/jenkins_home -v /var/run/docker.sock:/run/docker.sock ivantichy/jenkins
+
+
 #docker run -p 8082:8080 -p 50000:50000 -it -v /var/docker-data/jenkins-home:/var/jenkins_home ivantichy/jenkins
 
 
